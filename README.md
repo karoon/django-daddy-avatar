f7a552588513a5b022d752b431dea1c1797b627b
shellman <vchakoshy@gmail.com>
2013-01-26 22:25:25 +0330

    add usage

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 django-daddy-avatar
 ===================
 
-simple django avatar
+simple django avatar from gravatar
+
+Installation
+------------
+
+To install:
+
+    pip install django-daddy-avatar
+
+Usage:
+    
+    {% load daddy_avatar %}
+    <img src="{{'vchakoshy@gmail.com'|daddy_avatar:50}}" />
--- setup.py
+++ setup.py
@@ -10,9 +10,19 @@ setup(
     version = version,
     author = "vahid chakoshy",
     author_email = "vchakoshy@gmail.com",
-    description = "daddy avatar for pinterest apllication like in Django",
-    url = "http://www.wisgoon.com/",
+    description = "very simple avatar from gravatar",
+    url = "https://github.com/karoon/django-daddy-avatar",
     packages=find_packages(),
     include_package_data = True,
     zip_safe=False,
+    classifiers = [
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Natural Language :: Persian',
+        
+    ]
 )
